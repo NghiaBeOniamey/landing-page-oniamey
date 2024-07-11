4618b41e5cd98a28c8d26dafde1981255cb3db53
Oniamey <nghiabe.dev@gmail.com>
2024-07-11 11:41:25 +0700

    config build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/landing-page-oniamey/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(),],
+    base: "/landing-page-oniamey/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]'
+            }
+        }
+    },
 });
